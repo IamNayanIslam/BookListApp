e9e8ab06f61418fd5a9a5c33f4027d0e86f78211
IamNayanIslam <iamnayanislam@gmail.com>
2023-11-25 10:20:49 +0600

    done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+import Todo from "./Todo/Todo";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <Todo />
+      </div>
+    </>
+  );
+}
+
+export default App;
